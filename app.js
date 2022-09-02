0c1543e9786eab6b027b79234d6018a1730d4cc1
Nilesh <nnirgude.projects@gmail.com>
2022-09-02 17:21:16 +0530

    added changes to get info
    made some changes to get inputs by customers into the database

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mongoose = require("mongoose"); //including mongoose package
+mongoose.connect("mongodb://localhost/contactDance");
 const port = 8000;
+
+// Define Schema
+const contactSchema = new mongoose.Schema({
+  name: String,
+  phone: Number,
+  email: String,
+  address: String,
+  desc: String,
+});
+
+// Defining Model
+const Contact = mongoose.model("Contact", contactSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use("/static", express.static("static")); // For serving static files
@@ -14,7 +28,22 @@ app.set("views", path.join(__dirname, "views")); // Set the views directory
 // ENDPOINTS
 app.get("/", (req, res) => {
   const params = {};
-  res.status(200).render("index.pug", params);
+  res.status(200).render("home.pug", params);
+});
+
+app.get("/contact", (req, res) => {
+  const params = {};
+  res.status(200).render("contact.pug", params);
+});
+
+app.post("/contact", (req, res) => {
+  const contactData = new Contact(req.body);
+  contactData.save().then(() => {
+    res.send("This information has been saved to database.");
+  }).catch(() => {
+    res.status(400).send("This information was not saved to the database");
+  });
+  // res.status(200).render("contact.pug");
 });
 
 // START THE SERVER
